ab16786ddf7417857aaae0aff57c1ed4c6546da5
Peter Leriche <R0370657@student.thomasmore.be>
2017-01-03 22:19:08 +0100

    Code ovegenomen van Jeroen, heeft mij uitgelegd hoe het werkt.
    Kon ook gewoon per content een validationfile schrijven met heel veel ifs, maar dit is veel overzichtelijker.

--- Validation.js
+++ Validation.js
@@ -1,6 +1,14 @@
-//hier komt de inputvalidator
 module.exports{
-    NotEmpty: function(object){
-        
+    fieldsNotEmpty: function (object) {
+        var errors = [];
+        for (i = 1; i < arguments.length; i++) {
+            if (!this.fieldNotEmpty(object, arguments[i])) {
+                errors.push(arguments[i]);
+            }
+        };
+        return errors.length === 0 ? null : errors;
+        },
+    fieldNotEmpty: function (object, field) {
+        return object && object[field] && object[field] !== "";
     }
 };
