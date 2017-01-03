8153ac0e95acb098c2f5360c089e031195927cb6
Peter Leriche <R0370657@student.thomasmore.be>
2017-01-03 16:23:21 +0100

    voor Drones hebben we voornamelijk nodig:
    location naam, Id van drone, Date

--- DroneStorage.js
+++ DroneStorage.js
@@ -1,7 +1,19 @@
 var mongoose = require("mongoose");
 
 var DroneSchema = mongoose.Schema({
-    
+    Location: {
+        type: string,
+        required: true
+    },
+    id: {
+        type: string,
+        required: true,
+        unique: true
+    },
+    date: {
+        type: datetime,
+        required: true
+    }   
 });
 
 
