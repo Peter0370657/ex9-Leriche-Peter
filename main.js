8ca12883602671a5a96c2750c4489b4a3f7afe15
Peter Leriche <R0370657@student.thomasmore.be>
2017-01-03 16:04:11 +0100

    Npm install opnieuw gedaan aangezien vorige keer niet gelukt was npm install mongoose --save -prefix D:\.... cd d:\location werkt niet voor een of andere reden

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//opdracht 9
+//opdracht 9 
 
 var express = require('express');
 var parser = require('body-parser');
@@ -6,6 +6,6 @@ var parser = require('body-parser');
 var mongoose = require('mongoose'); //op aanraden van Jonas / Wibren
 mongoose.connect("mongodb://localhost/opdracht9");
 
-
+/
 
 console.log("hello world");
